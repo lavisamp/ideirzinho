778b345103020ca7d2c2553ca8cfda0869c505e9
lavisamp <lavinia.fernandes.sampaio@gmail.com>
2025-11-04 16:18:01 -0400

    funciono mais ou menos

--- tarefas.php
+++ tarefas.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-  <?php require_once "_parts/_menu2.php"; ?>
+  <?php require_once "_parts/_menu.php"; ?>
 
   <main class="container mt-4">
     <div class="d-flex justify-content-between align-items-center">
